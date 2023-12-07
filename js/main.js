c8bde0889557e20628db4590c7e8602aad35c8d5
Felix Aldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-07 08:54:26 +0000

    Finalised calculations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function init() {
     // Get all the inputs
-    const hoursPerWeekInput = document.querySelector('#hoursPerWeek');
     const currentSalaryInput = document.querySelector('#currentSalary');
     const commuteTimeHoursInput = document.querySelector('#commuteTimeHours');
     const commuteTimeMinutesInput = document.querySelector('#commuteTimeMinutes');
@@ -13,8 +12,8 @@ function init() {
     setupSelectedPercentageBoxes(percentageBoxes[3]); // Assuming the last box (100%) is the default
 
     // Add event listeners to inputs
-    [hoursPerWeekInput, currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput].forEach(input => {
-        input.addEventListener('input', () => calculateFinalValues(hoursPerWeekInput, currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes));
+    [currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput].forEach(input => {
+        input.addEventListener('input', () => calculateFinalValues(currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes));
     });
 
     // Add click event listeners to percentage boxes
@@ -23,36 +22,124 @@ function init() {
             // Mark the clicked box as selected
             percentageBoxes.forEach(b => b.classList.remove('selected'));
             box.classList.add('selected');
-            calculateFinalValues(hoursPerWeekInput, currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes);
+            calculateFinalValues(currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes);
         });
     });
 }
 
 
-function calculateFinalValues(hoursPerWeekInput, currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes) {
-    // Get the values from inputs
-    const hoursPerWeekValue = hoursPerWeekInput.value;
-    const currentSalaryValue = currentSalaryInput.value;
-    const commuteTimeHoursValue = commuteTimeHoursInput.value;
-    const commuteTimeMinutesValue = commuteTimeMinutesInput.value;
-    const fuelCostValue = fuelCostInput.value;
+function calculateFinalValues(currentSalaryInput, commuteTimeHoursInput, commuteTimeMinutesInput, fuelCostInput, percentageBoxes) {
+    // Get the values from inputs with default to "0" if blank or null
+    const currentSalaryValue = currentSalaryInput.value || "0";
+    const commuteTimeHoursValue = commuteTimeHoursInput.value || "0";
+    const commuteTimeMinutesValue = commuteTimeMinutesInput.value || "0";
+    const fuelCostValue = fuelCostInput.value || "0";
 
     // Find selected percentage box
     const selectedPercentageBox = Array.from(percentageBoxes).find(box => box.classList.contains('selected'));
     const percentageBoxValue = selectedPercentageBox ? selectedPercentageBox.innerText : null;
 
     console.log("Calculating final values with:", 
-        hoursPerWeekValue, 
         currentSalaryValue, 
         `${commuteTimeHoursValue}h ${commuteTimeMinutesValue}m`, 
         fuelCostValue, 
         percentageBoxValue);
 
-    // Continue with your calculation logic
-    // Update the output elements based on the calculations
-    // Example: document.querySelector('#outputElementId').innerText = calculatedValue;
+    // Calculation Logic
+    // 52 weeks a year * 5 = 260 working days - Bank Holidays(8) - 251 Days
+    // Assume 25 days of holiday used = 226 working days
+
+    const totalWorkingDays = 260;
+    const totalWorkingDaysMinusHoliday = totalWorkingDays - 33;
+
+    // Commute time
+    const hoursMinutesCommute = {
+        'hours': parseInt(commuteTimeHoursValue, 10), 
+        'minutes': parseInt(commuteTimeMinutesValue, 10)
+    };
+    hoursMinutesCommute.totalMinutesPerDay = (hoursMinutesCommute.hours * 60) + hoursMinutesCommute.minutes;
+    hoursMinutesCommute.totalMinutesPerYear = hoursMinutesCommute.totalMinutesPerDay * totalWorkingDaysMinusHoliday;
+
+    // Calculate worked time (minus holiday)
+    const workedTime = {'hours': 7, 'minutes': 24}; // These are already numbers
+    workedTime.totalMinutesPerDay = (workedTime.hours * 60) + workedTime.minutes;
+    workedTime.totalMinutesPerYear = workedTime.totalMinutesPerDay * totalWorkingDaysMinusHoliday;
+
+    // Calculate worked time including commute
+    const workedTimeIncludingCommute = {
+        'hours': workedTime.hours + hoursMinutesCommute.hours, 
+        'minutes': workedTime.minutes + hoursMinutesCommute.minutes
+    };
+    if (workedTimeIncludingCommute.minutes >= 60) {
+        workedTimeIncludingCommute.hours += Math.floor(workedTimeIncludingCommute.minutes / 60);
+        workedTimeIncludingCommute.minutes %= 60;
+    }
+    workedTimeIncludingCommute.totalMinutesPerDay = (workedTimeIncludingCommute.hours * 60) + workedTimeIncludingCommute.minutes;
+    workedTimeIncludingCommute.totalMinutesPerYear = workedTimeIncludingCommute.totalMinutesPerDay * totalWorkingDaysMinusHoliday;
+
+    const currentSalary = parseInt(currentSalaryValue, 10);
+    const currentSalaryPerDay = currentSalary / 226;
+    const costOfFuelPerDay = parseInt(fuelCostValue, 10) / 5;
+
+    // Calculate daily lost time in minutes
+    const lostTimeMinutesPerDay = (workedTimeIncludingCommute.totalMinutesPerDay) - (workedTime.totalMinutesPerDay);
+    const lostTime = {
+        'hours': Math.floor(lostTimeMinutesPerDay / 60),
+        'minutes': lostTimeMinutesPerDay % 60
+    };
+    lostTime.totalMinutesPerDay = (lostTime.hours * 60) + lostTime.minutes;
+    lostTime.totalMinutesPerYear = lostTime.totalMinutesPerDay * totalWorkingDaysMinusHoliday;
+
+    // Calculate Lost Money
+    const fuelCostPerYear = costOfFuelPerDay * totalWorkingDaysMinusHoliday;
+
+    // Calculate sallery per hour
+    const totalWorkingHoursPerYear = workedTime.totalMinutesPerYear / 60;
+    const salaryPerHour = currentSalary / totalWorkingHoursPerYear;
+    // Calculate sallery minus fuel with commute hours
+    const totalWorkingHoursPerYearIncludingCommute = workedTimeIncludingCommute.totalMinutesPerYear / 60;
+    const salaryMinusFuel = currentSalary - fuelCostPerYear;
+    const salaryPerHourIncludingCommuteFuel = salaryMinusFuel / totalWorkingHoursPerYearIncludingCommute;
+
+
+    // Calculate percentage sallery decrease
+    const percentageSalaryDecrease = ((salaryPerHour - salaryPerHourIncludingCommuteFuel) / salaryPerHour) * 100;
+
+    // Calculate additional carbon
+    const additionalCarbon = 10;
+
+    // calcualte hourly paycut
+    const hourlyPaycut = salaryPerHour - salaryPerHourIncludingCommuteFuel;
+
+    // Calculate
+    setOutputs(lostTime, fuelCostPerYear, additionalCarbon, percentageSalaryDecrease,
+         salaryMinusFuel, salaryPerHour, salaryPerHourIncludingCommuteFuel, hourlyPaycut);
+
 }
 
+function setOutputs(lostTime, lostMoney, additionalCarbon, percentageSalaryDecrease,
+     finalSalary, salaryPerHour, salaryPerHourTravelFuel, hourlyPaycut) {
+    const lostMoneyOutput = document.querySelector('#moneyOutput');
+    const additionalCarbonOutput = document.querySelector('#additionalCarbon');
+    const percentageSalaryDecreaseOutput = document.querySelector('#percentageSalleryDecrease');
+    const finalSalaryOutput = document.querySelector('#finalSallery');
+    const salaryPerHourOutput = document.querySelector('#salaryPerHourOutput');
+    const salaryPerHourTravelFuelOutput = document.querySelector('#salaryPerHourTravelFuelOutput');
+    const hourlyPaycutOutput = document.querySelector('#hourlyPaycut');
+
+    const lostTimeOutput = document.querySelector('#lostTime');
+    const lostHours = Math.floor(lostTime.totalMinutesPerYear / 60);
+    const lostMinutes = lostTime.totalMinutesPerYear % 60;
+    lostTimeOutput.textContent = `Hours: ${lostHours}, Minutes: ${lostMinutes}`;
+
+    lostMoneyOutput.textContent = lostMoney;
+    additionalCarbonOutput.textContent = additionalCarbon;
+    percentageSalaryDecreaseOutput.textContent = percentageSalaryDecrease + '%';
+    finalSalaryOutput.textContent = finalSalary;
+    salaryPerHourOutput.textContent = salaryPerHour;
+    salaryPerHourTravelFuelOutput.textContent = salaryPerHourTravelFuel;
+    hourlyPaycutOutput.textContent = hourlyPaycut;
+}
 
 function setupSelectedPercentageBoxes(percentageBox) {
     percentageBox.classList.add('selected');
