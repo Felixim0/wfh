c44c33a63e2f92c9a57208853f3bca4cada96c05
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-28 21:32:12 +0000

    Added criteria for PWA installation candidate

---
+++ worker.js
@@ -0,0 +1,38 @@
+const CACHE = 'wfh';
+const FILES = [
+  './',
+  './index.css',
+  './worker.js',
+  './js/main.js',
+  './icons/wfh-256.png',
+  './icons/wfh-512.png',
+];
+
+async function fetchFromCache(request) {
+  const cache = await caches.open(CACHE);
+  const data = await cache.match(request);
+  if (data) {
+    return data;
+  } else {
+    await cache.add(request);
+    return cache.match(request);
+  }
+}
+
+function interceptFetch(e) {
+  e.respondWith(fetchFromCache(e.request));
+}
+
+async function installMyServiceWorker() {
+  // pre-cache everything
+  try {
+    const c = await caches.open(CACHE);
+    await c.addAll(FILES);
+    console.log('Cache is primed.');
+  } catch (e) {
+    console.warn("Priming cache failed.  Falling back to 'online only'.", e);
+  }
+}
+
+self.addEventListener('install', installMyServiceWorker);
+self.addEventListener('fetch', interceptFetch);
